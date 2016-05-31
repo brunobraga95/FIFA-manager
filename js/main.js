e1a5b3d45aec2fef2df243505d8aba471d3f57ce
Tiago Alves Dulce <tiagoalvesdulce@tiagos-mbp.dhcp.nd.edu>
2016-05-30 20:01:52 -0400

    comeco da funcionalidade do web app

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 $(function(){
+	var partidas = 0;
 	$('#resumo').click(function(){
 		$(this).addClass('active');
 		$(this).siblings().removeClass('active');
-		$('.canvas-container').removeClass('hidden');
-		$('.recente-wrapper').addClass('hidden');
+		if (partidas != 0) {
+			$('.canvas-container').removeClass('hidden');
+			$('.recente-wrapper').addClass('hidden');
+		}
 		$('.main-header > h1 > small').html('Resumo');
 		$('.main-sub-header').removeClass('hidden');
 
@@ -11,26 +14,66 @@ $(function(){
 	$('#recente').click(function(){
 		$(this).addClass('active');
 		$(this).siblings().removeClass('active');
-		$('.canvas-container').addClass('hidden');
-		$('.recente-wrapper').removeClass('hidden');
+		if(partidas != 0){
+			$('.canvas-container').addClass('hidden');
+			$('.recente-wrapper').removeClass('hidden');
+		}
 		$('.main-header > h1 > small').html('Recente');
 		$('.main-sub-header').addClass('hidden');
+		
 	});
 	
-	$('#todos-grupos li, #todos-amigos li, #geral li').click(function(event){
+	$('#todos-grupos, #todos-amigos, #geral').on('click', 'li', function(event){
 		var elemId = $(this).attr('id');
-		console.log(elemId);
 		if(elemId != undefined){
 			if($('#resumo').hasClass('active')){
-				var str = '<h1>' + elemId.substring(12) + '<small>Resumo</small></h1>'
+				var str = underlineToSpaces(elemId.substring(12)) + '<small>Resumo</small>'
 			}else{
-				var str = '<h1>' + elemId.substring(12) + '<small>Recente</small></h1>'
+				var str = underlineToSpaces(elemId.substring(12)) + '<small>Recente</small>'
 			}
-			$('.main-header').html(str);
+			$('.main-header h1').html(str);
 			$('#todos-amigos li, #todos-grupos li, #geral li').removeClass('active');
 			$(this).addClass('active');
 		}
 	});
 
-	$()
+	function popupAux(event){
+		event.preventDefault();
+		$.magnificPopup.close();
+	}
+
+	function spacesToUnderline(val){
+		return val.replace(/\s/g, '_');
+	}
+
+	function underlineToSpaces(val){
+		return val.replace(/_/g,' ');
+	}
+
+	$('#add_novo_grupo_btn').click(function(event){
+		popupAux(event);
+		$('#todos-grupos').append("<li class='list-group-item' id=" + 'fifa-grupos-' + spacesToUnderline(novo_grupo_nome.value) + ">" + novo_grupo_nome.value + "</li>");
+		$('#todos-grupos li:last-child').trigger('click');
+	});
+	$('#add_nova_partida_btn').click(function(event){
+		popupAux(event);
+		$('.recente-wrapper').append("<div><h5>" + adversario_nome.value + "</h5><h6>" + usuario_time.value +" " + usuario_gols.value + " x "+ adversario_gols.value + "  "+ adversario_time.value + " </h6></div>");
+		if (usuario_gols.value > adversario_gols.value) {estatisticas[0]++;}
+		if (usuario_gols.value == adversario_gols.value) {estatisticas[1]++;}
+		if (usuario_gols.value < adversario_gols.value) {estatisticas[2]++;}
+		partidas++;
+		$('.nenhuma-partida').addClass('hidden');
+		if($('#resumo').hasClass('active')){
+			$('.canvas-container').removeClass('hidden');
+		}else{
+			$('.recente-wrapper').removeClass('hidden');
+		}
+		graficoResumoGeral.data.datasets[0].data = estatisticas;
+		graficoResumoGeral.update();
+	});
+	$('#add_amigos_btn').click(function(event){
+		popupAux(event);
+		$('#todos-amigos').append("<li class='list-group-item' id=" + 'fifa-amigos-' + spacesToUnderline(amigo_nome.value) + ">" + amigo_nome.value + "</li>");
+		$('#todos-amigos li:last-child').trigger('click');
+	});
 });
