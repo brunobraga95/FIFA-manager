bdd6e00bb55e08d945d6566e4bd7ffd55fceb386
brunobraga95 <brn95@live.com>
2016-07-22 12:36:00 -0700

    python parse script debuged

--- files/parseTeams.py
+++ files/parseTeams.py
@@ -8,13 +8,13 @@ with open('fifa15-16teams') as f:
     lines = f.readlines()
 res = []
 teams = []
-divisionName = ''
+divisionName = '\''
 for i in lines:
 	if i[0] == '[':
-		if divisionName!='':
+		if divisionName!='\'':
 			res.append(teams)
 		teams = []
-		divisionName = ''
+		divisionName = '\''
 		for j in i:
 			if j!='[' and j!=']':
 				divisionName = divisionName+j
@@ -22,12 +22,14 @@ for i in lines:
 				if j==']':	 
 					divisionName = divisionName+' '
 		divisionName = divisionName[:-1]
+		divisionName = divisionName+'\''
 		teams.append(divisionName)
 	else:
-		time = i
+		time = '\''+i
 		tam = len(time)
 		if time[tam-1] == '\n':
 			time = time[:-1]
+		time = time+'\''
 		teams.append(time)
 res.append(teams)
 string = '[['
